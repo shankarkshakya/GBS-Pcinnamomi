4f80058201a03ae86355e8f625d274e6ef3c61e1
Shankar Kaji Shakya <shankarkshakya@users.noreply.github.com>
2017-06-30 10:59:48 -0700

    Add files via upload

--- vcfR2migrate.R
+++ vcfR2migrate.R
@@ -1,8 +1,108 @@
 
-library(vcfR)
+vcfR2migrate <- function(vcf, pop, in_pop, out_file) {
+  
+  #Removing indels and multiallelic loci
+  vcf <- extract.indels(vcf, return.indels = F) 
+  vcf <- vcf[is.biallelic(vcf)]
+  
+  gt <- extract.gt(vcf, return.alleles = T, convertNA = T)
+  gt <- gt[!rowSums((is.na(gt))),]
+  
+  FORMAT <- vcf@gt[1:nrow(gt),1]
+  vcf@gt <- cbind(FORMAT, gt)
+  
+  
+  vcf_list <- vector("list", length(unique(pop)))
+  names(vcf_list) <- unique(pop)
+  
+        for (i in (1:length(vcf_list))) {
+          temp_pop <- names(vcf_list[i])
+          temp_vcf <- vcf
+          FORMAT <- vcf@gt[,1]
+          gt <- temp_vcf@gt[, -1]
+          
+          cols <- gt[ , which(names(vcf_list[i]) == pop)]
+          
+          temp_vcf@gt <- cbind(FORMAT, cols)
+          
+          
+          vcf_list[[i]] <- temp_vcf
+          
+        }
+  
+  n.pop <- vector("list", length(vcf_list))
+  names(n.pop) <- names(vcf_list)
+        for ( i in 1:length(vcf_list)) {
+          
+          pop1 <- vcf_list[[i]]
+          
+          gt <- pop1@gt
+          
+          all <- vector("list", nrow(gt))
+          
+                for (j in 1:nrow(gt)){
+                  ind <- gt[j, -1]
+                  all[[j]] <- as.data.frame(unlist(strsplit(ind, "\\||/")))
+                }
+          
+          n.pop[[i]] <- do.call("rbind", all)
+          
+        }
+  
+  
+migrate_pop <- n.pop
+
+for (i in 1:length(n.pop)) {
+  temp.pop <- n.pop[[i]]
+  names <- rownames(temp.pop)
+  rownames(temp.pop) <- NULL
+  temp.pop <- cbind(names,temp.pop)
+  indnum <- nrow(temp.pop)/ (nrow(vcf@gt))
+  colnames(temp.pop) <- c(indnum, names(n.pop)[[i]])
+  
+  migrate_pop[[i]] <- temp.pop
+  
+  
+  
+}
+  
+loci_length <- rep(1, nrow(vcf@gt))
+
+x <- list(loci_length = loci_length, migrate_pop = migrate_pop)
+
+in_pop <- in_pop
+output_path <- file.path(out_file)
+data_to_write <- x$migrate_pop[sapply(x$migrate_pop, function(x) any(colnames(x) %in% in_pop))]
+
+# write header
+header <- paste0("N ", length(data_to_write), " ", length(x$loci_length), "\n", paste(x$loci_length, collapse = " "))
+writeLines(header, output_path)
+
+# write data
+for (frame in data_to_write) {
+  write.table(frame, file = output_path, append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+}
+
+  
+}
+
+
+
+
+
+pkg <- "pinfsc50"
+my_vcf <- system.file("extdata", "pinf_sc50.vcf.gz", package = pkg)
+my_vcf <- read.vcfR( vcf_file, verbose = FALSE )
+
+my_pop <- as.factor(rep(c("A", "B", "C"), each = 6))
+
+
+
+vcfR2migrate(vcf = my_vcf, pop = my_pop, in_pop = c("A", "C"))
+
+
 
 vcf <- read.vcfR("MAF_Filtered.vcf.gatk6262.gz", verbose = FALSE)
-
 pop_vec <- structure(c(7L, 7L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 7L, 7L, 9L, 9L, 
                        7L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 
                        9L, 9L, 9L, 9L, 9L, 7L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
@@ -15,92 +115,8 @@ pop_vec <- structure(c(7L, 7L, 9L, 9L, 9L, 9L, 9L, 9L, 9L, 7L, 7L, 9L, 9L,
                                                            "France", "Italy", "PNG", "Portugal", "South Africa", "Taiwan", 
                                                            "Vietnam"), class = "factor")
 
-vcfR2migrate <- function(vcf, pop) {
-
-vcf <- extract.indels(vcf, return.indels = F) #Removing indels
-vcf <- vcf[is.biallelic(vcf)]
-
-gt <- extract.gt(vcf, return.alleles = T, convertNA = T)
-gt <- gt[!rowSums((is.na(gt))),]
-
-FORMAT <- vcf@gt[1:nrow(gt),1]
-vcf@gt <- cbind(FORMAT, gt)
-
-
-vcf_list <- vector("list", length(unique(pop_vec)))
-names(vcf_list) <- unique(pop_vec)
- 
-for (i in (1:length(vcf_list))) {
-  temp_pop <- names(vcf_list[i])
-  temp_vcf <- vcf
-  FORMAT <- vcf@gt[,1]
-  gt <- temp_vcf@gt[, -1]
-  
-  cols <- gt[ , which(names(vcf_list[i]) == pop_vec)]
-  
-  temp_vcf@gt <- cbind(FORMAT, cols)
-  
- 
-  vcf_list[[i]] <- temp_vcf
-
-}
-
-n.pop <- vector("list", length(vcf_list))
-names(n.pop) <- names(vcf_list)
-for ( i in 1:length(vcf_list)) {
-
-    pop1 <- vcf_list[[i]]
-
-    gt <- pop1@gt
-
-    all <- vector("list", nrow(gt))
-      
-    for (j in 1:nrow(gt)){
-        ind <- gt[j, -1]
-        all[[j]] <- as.data.frame(unlist(strsplit(ind, "/")))
-    }
-    
-n.pop[[i]] <- do.call("rbind", all)
-
-}
-
-
-migrate_pop <- n.pop
-  
-for (i in 1:length(n.pop)) {
-  temp.pop <- n.pop[[i]]
-  names <- rownames(temp.pop)
-  rownames(temp.pop) <- NULL
-  temp.pop <- cbind(names,temp.pop)
-  indnum <- nrow(temp.pop)/ (2*nrow(vcf@gt))
-  colnames(temp.pop) <- c(indnum, names(n.pop)[[i]])
-  
-  migrate_pop[[i]] <- temp.pop
-  
-  #write.table(temp.pop, file = paste(names(n.pop[i]), ".txt", sep = ""), sep = "\t", row.names = F, quote = F)
-  
-}
-
-loci_length <- rep(1, nrow(vcf@gt))
-
-return(list(loci_length = loci_length, migrate_pop = migrate_pop))
-
-#return(migrate_pop)
-
-}
 
 
 
+vcfR2migrate(vcf = vcf , pop = pop_vec , in_pop = c("Australia", "Taiwan", "Vietnam"), out_file = "my3pop.txt")
 
-contries <- c("Taiwan", "Australia")
-output_path <- file.path("test.txt")
-data_to_write <- x$migrate_pop[sapply(x$migrate_pop, function(x) any(colnames(x) %in% contries))]
-
-# write header
-header <- paste0("N ", length(data_to_write), " ", length(x$loci_length), "\n", paste(x$loci_length, collapse = " "))
-writeLines(header, output_path)
-
-# write data
-for (frame in data_to_write) {
-  write.table(frame, file = output_path, append = TRUE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
-}
