d959cc2a20a57d415950569cf79e9be268778946
shankarkshakya <kaji_shakya@yahoo.com>
2018-08-25 11:26:04 -0700

    color modified

--- Fig2_worldmap.R
+++ Fig2_worldmap.R
@@ -17,20 +17,26 @@ malMap <- joinCountryData2Map(malDF, joinCode = "ISO3",
 malMap <- subset(malMap, malMap@data$NAME != "Antarctica")
 
 #tiff("./FIGS/worldmap.tiff", width = 11.5, height = 7, units = "in", res = 600)
+ 
+
+sort(theCountries)
+mycol <- pals::alphabet(n =16)
+mycol <- c("#FFFF00","#ff0000" , "#FFC0CB", "#00FFFF","#0000FF",
+           rep("#ff0000", 6), "#0000FF", "#00ff00",  "#00FFFF", "#FF00FF", "#0000FF")
 
 mapCountryData(malMap, nameColumnToPlot = "country", missingCountryCol = "gray", 
                catMethod = "categorical",
-              colourPalette = rep("#89080e", 16), xlim = c(0,37), addLegend = F,
-               #colourPalette = pals::alphabet(n =16), xlim = c(0,37), addLegend = F,
+              colourPalette = mycol, xlim = c(0,37), addLegend = F,
               mapTitle = NA)
 
 #dev.off()
 
-
-
-
-
-
-
+# Europe = "#ff0000", 
+# taiwan =   "#00ff00",
+#   australia = "#FFFF00",
+#   africa = "#0000FF",
+#   north america = "#00FFFF",
+#   vietnam= "#FF00FF",
+#   south america = "#FFC0CB"
 
 
